c68ce29f52f898339b7aae47906eec5e43f6c61f
Vliedel <bart@dobots.nl>
2016-05-16 00:05:56 +0200

    initial commit

---
+++ overviewer_conf.py
@@ -0,0 +1,104 @@
+worlds["lastingland"] = "/home/minecraft/mc/lastingland"
+texturepath = "/home/minecraft/textures"
+#outputdir = "/home/minecraft/overviewer"
+outputdir = "/var/www/html/overviewer"
+processes = 1
+
+def signFilter(poi):
+	# Return (hover text, window content text)
+	if poi['id'] == 'Sign':
+		# Signs have Text1 - Text4
+		if (poi['Text1'].startswith("[home]")):
+			poi["icon"] = "icons/marker_tower.png"
+			return "\n".join([poi['Text1'], poi['Text2'], poi['Text3'], poi['Text4']])
+		if (poi['Text1'].startswith("[farm]")):
+			poi["icon"] = "icons/marker_factory_red.png"
+			return "\n".join([poi['Text1'], poi['Text2'], poi['Text3'], poi['Text4']])
+		if (poi['Text1'].startswith("[claim]")):
+			poi["icon"] = "icons/marker_mine_red.png"
+			return "\n".join([poi['Text1'], poi['Text2'], poi['Text3'], poi['Text4']])
+		if (poi['Text1'].startswith("[waypoint]")):
+#			poi["icon"] = ""
+			return "\n".join([poi['Text1'], poi['Text2'], poi['Text3'], poi['Text4']])
+		if (poi['Text1'].startswith("[city]")):
+			poi["icon"] = "icons/marker_town.png"
+			return "\n".join([poi['Text1'], poi['Text2'], poi['Text3'], poi['Text4']])
+		return None
+
+def playerIcons(poi):
+	if poi['id'] == 'Player':
+		poi['icon'] = "http://overviewer.org/avatar/%s" % poi['EntityId']
+		return "%s" % poi['EntityId']
+
+end_lighting = [Base(), EdgeLines(), Lighting(strength=0.5)]
+end_smooth_lighting = [Base(), EdgeLines(), SmoothLighting(strength=0.5)]
+
+renders["end"] = {
+	"world": "lastingland",
+	"title": "The End",
+	"rendermode": end_smooth_lighting,
+#	"rendermode": [ClearBase(), BiomeOverlay()],
+	"dimension": "end",
+	"markers": [dict(name="Players", filterFunction=playerIcons, createInfoWindow=False, checked=True)],
+#	"poititle": "Overlays",
+	"showspawn": False,
+}
+
+
+renders["promisedland-day"] = {
+	"world": "lastingland",
+	"title": "Promised Land - Day",
+	"rendermode": smooth_lighting,
+	"dimension": "DIM20",
+	"markers": [dict(name="Players", filterFunction=playerIcons, createInfoWindow=False, checked=True)],
+#	"poititle": "Overlays",
+	"showspawn": False,
+}
+#renders["promisedland-night"] = {
+#	"world": "lastingland",
+#	"title": "Promised Land - Night",
+#	"rendermode": smooth_night,
+#	"dimension": "DIM20",
+#	"markers": [dict(name="Players", filterFunction=playerIcons, createInfoWindow=False, checked=True)],
+#	"poititle": "Overlays",
+#	"showspawn": False,
+#}
+
+renders["overworld-day"] = {
+	"world": "lastingland",
+	"title": "Overworld - Day",
+	"rendermode": smooth_lighting,
+	"dimension": "overworld",
+	"markers": [
+		dict(name="Players", filterFunction=playerIcons, createInfoWindow=False, checked=True),
+		dict(name="Signs", filterFunction=signFilter, createInfoWindow=False, checked=True)
+	],
+#	"poititle": "Overlays",
+	"showspawn": True,
+	"northdirection": "upper-left",
+}
+
+renders["overworld-day"] = {
+	"world": "lastingland",
+	"title": "Overworld - Day 2",
+	"rendermode": smooth_lighting,
+	"dimension": "overworld",
+	"markers": [
+		dict(name="Players", filterFunction=playerIcons, createInfoWindow=False, checked=True),
+		dict(name="Players", filterFunction=signFilter, createInfoWindow=False, checked=True)
+	],
+#	"poititle": "Overlays",
+	"showspawn": True,
+	"northdirection": "lower-right",
+}
+
+renders["nether"] = {
+	"world": "lastingland",
+	"title": "Nether",
+	"rendermode": nether_smooth_lighting,
+	"dimension": "nether",
+	"markers": [dict(name="Players", filterFunction=playerIcons, createInfoWindow=False, checked=True)],
+#	"poititle": "Overlays",
+	"showspawn": False,
+}
+
---
+++ overviewer_test.py
@@ -0,0 +1,58 @@
+worlds["lastingland"] = "/home/minecraft/mc/lastingland"
+outputdir = "/var/www/html/overviewer-test"
+processes = 1
+
+def playerIcons(poi):
+    if poi['id'] == 'Player':
+        poi['icon'] = "http://overviewer.org/avatar/%s" % poi['EntityId']
+        return "%s" % poi['EntityId']
+
+def signFilter(poi):
+	# Return (hover text, window content text)
+	if poi['id'] == 'Sign':
+		# Signs have Text1 - Text4
+		if (poi['Text1'].startswith("[home]")):
+			poi["icon"] = "marker_tower.png"
+			return "\n".join([poi['Text1'], poi['Text2'], poi['Text3'], poi['Text4']])
+		if (poi['Text1'].startswith("[farm]")):
+			poi["icon"] = "marker_factory_red.png"
+			return "\n".join([poi['Text1'], poi['Text2'], poi['Text3'], poi['Text4']])
+		if (poi['Text1'].startswith("[claim]")):
+			poi["icon"] = "marker_mine_red.png"
+			return "\n".join([poi['Text1'], poi['Text2'], poi['Text3'], poi['Text4']])
+		if (poi['Text1'].startswith("[waypoint]")):
+#			poi["icon"] = ""
+			return "\n".join([poi['Text1'], poi['Text2'], poi['Text3'], poi['Text4']])
+		if (poi['Text1'].startswith("[city]")):
+			poi["icon"] = "marker_town.png"
+			return "\n".join([poi['Text1'], poi['Text2'], poi['Text3'], poi['Text4']])
+		return None
+
+end_lighting = [Base(), EdgeLines(), Lighting(strength=0.5)]
+end_smooth_lighting = [Base(), EdgeLines(), SmoothLighting(strength=0.5)]
+
+renders["end"] = {
+	"world": "lastingland",
+	"title": "End",
+	"rendermode": end_smooth_lighting,
+	"dimension": "end",
+	"markers": [
+		dict(name="Players", filterFunction=playerIcons, createInfoWindow=False, checked=True),
+		dict(name="Players", filterFunction=signFilter, createInfoWindow=False, checked=True)
+	],
+	"poititle": "Overlays",
+	"showspawn": False,
+	"defaultzoom": 1, # 0-8
+}
+
+
+#renders["nether"] = {
+#	"world": "lastingland",
+#	"title": "Nether",
+#	"rendermode": nether_smooth_lighting,
+#	"dimension": "overworld",
+#	"markers": [dict(name="Players", filterFunction=playerIcons, createInfoWindow=False, checked=True)],
+#	"poititle": "Overlays",
+#	"showspawn": False,
+#}
+
